a273f56fcbbc445f736e71298aaf5d0d97cb0f5f
eprendre <eprendre0413@gmail.com>
2019-10-13 17:16:46 +0800

    refactor: 重构56听书网的分类

--- app/src/main/java/com/github/eprendre/tingshu/sources/impl/M56TingShu.kt
+++ app/src/main/java/com/github/eprendre/tingshu/sources/impl/M56TingShu.kt
@@ -16,27 +16,37 @@ import java.net.URLEncoder
 
 object M56TingShu : TingShu {
     override fun getCategoryMenus(): List<CategoryMenu> {
-        val menu1 = CategoryMenu(
-            "小说", R.drawable.ic_library_books, View.generateViewId(), listOf(
-                CategoryTab("玄幻武侠", "http://m.ting56.com/paihangbang/1-1.html"),
-                CategoryTab("都市言情", "http://m.ting56.com/paihangbang/2-2.html"),
-                CategoryTab("恐怖悬疑", "http://m.ting56.com/paihangbang/3-3.html"),
-                CategoryTab("网游竞技", "http://m.ting56.com/paihangbang/4-4.html"),
-                CategoryTab("军事历史", "http://m.ting56.com/paihangbang/6-6.html"),
-                CategoryTab("刑侦推理", "http://m.ting56.com/paihangbang/41-41.html")
-            )
+        val menu1 = CategoryMenu("有声小说", R.drawable.ic_library_books, View.generateViewId(), listOf(
+            CategoryTab("玄幻武侠", "http://m.ting56.com/book/1.html"),
+            CategoryTab("都市言情", "http://m.ting56.com/book/2.html"),
+            CategoryTab("恐怖悬疑", "http://m.ting56.com/book/3.html"),
+            CategoryTab("综艺娱乐", "http://m.ting56.com/book/45.html"),
+            CategoryTab("网游竞技", "http://m.ting56.com/book/4.html"),
+            CategoryTab("军事历史", "http://m.ting56.com/book/6.html"),
+            CategoryTab("刑侦推理", "http://m.ting56.com/book/41.html")
         )
-        val menu2 = CategoryMenu(
-            "其它", R.drawable.ic_more_horiz, View.generateViewId(), listOf(
-                CategoryTab("职场商战", "http://m.ting56.com/paihangbang/7-7.html"),
-                CategoryTab("百家讲坛", "http://m.ting56.com/paihangbang/10-10.html"),
-                CategoryTab("广播剧", "http://m.ting56.com/paihangbang/40-40.html"),
-                CategoryTab("幽默笑话", "http://m.ting56.com/paihangbang/44-44.html"),
-                CategoryTab("相声", "http://m.ting56.com/book/43.html"),
-                CategoryTab("儿童读物", "http://m.ting56.com/paihangbang/11-11.html")
-            )
         )
-        return listOf(menu1, menu2)
+        val menu2 = CategoryMenu("评书", R.drawable.ic_radio_black_24dp, View.generateViewId(), listOf(
+            CategoryTab("单田芳", "http://m.ting56.com/byy/shantianfang.html"),
+            CategoryTab("刘兰芳", "http://m.ting56.com/byy/liulanfang.html"),
+            CategoryTab("袁阔成", "http://m.ting56.com/byy/yuankuocheng.html"),
+            CategoryTab("田连元", "http://m.ting56.com/byy/tianlianyuan.html"),
+            CategoryTab("连丽如", "http://m.ting56.com/byy/lianliru.html"),
+            CategoryTab("王玥波", "http://m.ting56.com/byy/wangyuebo.html"),
+            CategoryTab("孙一", "http://m.ting56.com/byy/sunyi.html"),
+            CategoryTab("更多", "http://m.ting56.com/book/9.html")
+        )
+        )
+        val menu3 = CategoryMenu("其它", R.drawable.ic_more_horiz, View.generateViewId(), listOf(
+            CategoryTab("职场商战", "http://m.ting56.com/book/7.html"),
+            CategoryTab("百家讲坛", "http://m.ting56.com/book/10.html"),
+            CategoryTab("广播剧", "http://m.ting56.com/book/40.html"),
+            CategoryTab("幽默笑话", "http://m.ting56.com/book/44.html"),
+            CategoryTab("相声", "http://m.ting56.com/book/43.html"),
+            CategoryTab("儿童读物", "http://m.ting56.com/book/11.html")
+        )
+        )
+        return listOf(menu1, menu2, menu3)
     }
 
     override fun getAudioUrlExtractor(): AudioUrlExtractor {
@@ -76,18 +86,17 @@ object M56TingShu : TingShu {
 
     override fun getCategoryDetail(url: String): Single<Category> {
         return Single.fromCallable {
-            var currentPage: Int
-            var totalPage: Int
+            var currentPage = 1
+            var totalPage = 1
 
             val list = ArrayList<Book>()
             val doc = Jsoup.connect(url).get()
-            val container = doc.selectFirst(".xsdz")
-            doc.getElementById("page_num1").text().split("/").let {
+            doc.getElementById("page_num1")?.text()?.split("/")?.let {
                 currentPage = it[0].toInt()
                 totalPage = it[1].toInt()
             }
-            val nextUrl = doc.getElementById("page_next1").attr("abs:href")
-            val elementList = container.getElementsByClass("list-ov-tw")
+            val nextUrl = doc.getElementById("page_next1")?.attr("abs:href") ?: ""
+            val elementList = doc.getElementsByClass("list-ov-tw")
             elementList.forEach { item ->
                 var coverUrl = item.selectFirst(".list-ov-t a img").attr("original")
                 if (coverUrl.startsWith("/")) {//有些网址已拼接好，有些没有拼接
---
+++ app/src/test/java/com/github/eprendre/tingshu/M56TingShuTest.kt
@@ -0,0 +1,36 @@
+package com.github.eprendre.tingshu
+
+import assertk.assertThat
+import assertk.assertions.isGreaterThan
+import com.github.eprendre.tingshu.utils.Book
+import com.github.eprendre.tingshu.utils.Episode
+import org.jetbrains.anko.collections.forEachWithIndex
+import org.jsoup.Jsoup
+import org.junit.Test
+import java.lang.Exception
+import java.net.URLEncoder
+
+class M56TingShuTest {
+
+    @Test
+    fun fetchCategory() {
+        val doc = Jsoup.connect("http://m.ting56.com/mulu.html").get()
+        val box = doc.select(".chan_box")
+        box.forEachWithIndex { i, element ->
+            val sb = StringBuilder()
+            sb.append("val menu${i+1} = CategoryMenu(")
+            val title = element.selectFirst("h2").text()
+            sb.append("\"$title\", R.drawable.ic_library_books, View.generateViewId(), listOf(\n")
+
+            val list = element.select(".pd_class > li > a").map { a ->
+                val href = a.absUrl("href")
+                val text = a.text()
+                return@map "CategoryTab(\"$text\", \"$href\")"
+            }.joinToString(",\n")
+
+            sb.append(list)
+            sb.append("\n)\n)")
+            println(sb.toString())
+        }
+    }
+}
